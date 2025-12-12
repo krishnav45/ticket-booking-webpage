ce70ba4b98243357d4b2e907a50ea01bbef4c512
krishnav kishor <krishnav.k7k7@gmail.com>
2025-12-12 22:35:34 +0530

    backend ready for render

--- ticket-booking-backend/db.js
+++ ticket-booking-backend/db.js
@@ -1,4 +1,18 @@
-const { Pool } = require('pg');  // Import PostgreSQL client
+// const { Pool } = require('pg');  // Import PostgreSQL client
+// require('dotenv').config();       // Load .env variables
+
+// // Create a new pool to connect to Postgres
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASS,
+//     port: process.env.DB_PORT,
+ 
+// });
+
+// module.exports = pool;  // Export to use in other files
+ const { Pool } = require('pg');  // Import PostgreSQL client
 require('dotenv').config();       // Load .env variables
 
 // Create a new pool to connect to Postgres
@@ -8,6 +22,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false // REQUIRED for Render PostgreSQL
+    }
 });
 
 module.exports = pool;  // Export to use in other files
